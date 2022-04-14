99c7a637a9123d3017d80f90a634c5b32a31a480
Giacomo <29896316+Giacomo1995@users.noreply.github.com>
2022-04-14 13:29:04 +0200

    Small refactoring

--- Experiments/DL/utils.py
+++ Experiments/DL/utils.py
@@ -2,20 +2,20 @@
 import numpy as np
 
 
-def get_header(training_set):
+def get_header(dataset):
     """
-    It gets the training set, then saves and returns its header (i.e. the set of features).
+    It gets the dataset, then saves and returns its header (i.e. the set of features).
     """
 
-    header = np.array(training_set.columns)
+    header = np.array(dataset.columns)
     np.save('header.npy', header)  # Save header
 
     return header
 
 
-def filter_test_set(test_set, header=[], header_name='header.npy'):
+def filter_dataset(dataset, header=[], header_name='header.npy'):
     """
-    It filters the test set by projecting the indicated features (header) through parameter value or file name.
+    It filters the dataset by projecting the indicated features (header) through parameter value or file name.
     """
 
     # If the header parameter is left empty, load it by header_name
@@ -24,6 +24,6 @@ def filter_test_set(test_set, header=[], header_name='header.npy'):
     if header == []:
         header = np.load(header_name, allow_pickle=True)
 
-    test_set = test_set[header]  # Project the features on the test set
+    dataset = dataset[header]  # Project the features on the dataset
     
-    return test_set
+    return dataset
