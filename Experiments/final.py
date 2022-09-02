1be356746bd33ac4a27687eafaad52c6bba9361a
Giacomo <29896316+Giacomo1995@users.noreply.github.com>
2022-09-02 19:28:13 +0200

    Refactoring

--- Experiments/final.py
+++ Experiments/final.py
@@ -18,8 +18,8 @@ from sklearn import svm, ensemble
 
 # Flags
 USE_RAW_DATA = False
-USE_TEST_SET = True
-FEATURE_SELECTION = False
+USE_TEST_SET = False
+FEATURE_SELECTION = True
 CUSTOM_FEATURE_SELECTION = True
 
 
@@ -33,15 +33,17 @@ print("Reading validation set...")
 validation_set = pd.read_csv(os.path.join("Data", "validation_set.csv"), compression=None)
 print("Validation set loaded correctly.")
 
-# Read test set
-print("Reading test set...")
-test_set = pd.read_csv(os.path.join("Data", "test_set.csv"), compression=None)
-print("Test set loaded correctly.")
+if USE_TEST_SET:
+    # Read test set
+    print("Reading test set...")
+    test_set = pd.read_csv(os.path.join("Data", "test_set.csv"), compression='None)
+    print("Test set loaded correctly.")
 
 
 print("Training set shape:", training_set.shape)
 print("Validation set shape:", validation_set.shape)
-print("Test set shape:", test_set.shape)
+if USE_TEST_SET:
+    print("Test set shape:", test_set.shape)
 
 
 # Conversion from pandas dataframe to numpy array
@@ -54,9 +56,10 @@ y_val = np.array(validation_set["12210"]).astype(int)
 del validation_set["12210"]
 X_val = np.array(validation_set)
 
-y_test = np.array(test_set["12210"]).astype(int)
-del test_set["12210"]
-X_test = np.array(test_set)
+if USE_TEST_SET:
+    y_test = np.array(test_set["12210"]).astype(int)
+    del test_set["12210"]
+    X_test = np.array(test_set)
 
 
 # Standardization
@@ -64,7 +67,8 @@ print("Standardization...")
 std_scaler = StandardScaler()
 X_train_standardized = std_scaler.fit_transform(X_train)
 X_val_standardized = std_scaler.transform(X_val)
-X_test_standardized = std_scaler.transform(X_test)
+if USE_TEST_SET:
+    X_test_standardized = std_scaler.transform(X_test)
 
 
 if FEATURE_SELECTION:
@@ -77,47 +81,55 @@ if FEATURE_SELECTION:
             # Raw data
             X_train = (X_train[:, header])[:, :3000]
             X_val = (X_val[:, header])[:, :3000]
-            X_test = (X_test[:, header])[:, :3000]
+            if USE_TEST_SET:
+                X_test = (X_test[:, header])[:, :3000]
 
             print("Raw Data")
             print("X_train shape:", X_train.shape)
             print("X_val shape:", X_val.shape)
-            print("X_test shape:", X_test.shape)
+            if USE_TEST_SET:
+                print("X_test shape:", X_test.shape)
             print("\n")
         else:
             # Standardized data
             X_train_standardized = (X_train_standardized[:, header])[:, :3000]
             X_val_standardized = (X_val_standardized[:, header])[:, :3000]
-            X_test_standardized = (X_test_standardized[:, header])[:, :3000]
+            if USE_TEST_SET:
+                X_test_standardized = (X_test_standardized[:, header])[:, :3000]
 
             print("Standardized Data")
             print("X_train_standardized shape:", X_train_standardized.shape)
             print("X_val_standardized shape:", X_val_standardized.shape)
-            print("X_test_standardized shape:", X_test_standardized.shape)
+            if USE_TEST_SET:
+                print("X_test_standardized shape:", X_test_standardized.shape)
     else:  # Standard Feature Selection
         if USE_RAW_DATA:
             # Raw Data
             selector = SelectKBest(mutual_info_classif, k=3000)
             X_train = selector.fit_transform(X_train, y_train)
             X_val = selector.transform(X_val)
-            X_test = selector.transform(X_test)
+            if USE_TEST_SET:
+                X_test = selector.transform(X_test)
 
             print("Raw Data")
             print("X_train shape:", X_train.shape)
             print("X_val shape:", X_val.shape)
-            print("X_test shape:", X_test.shape)
+            if USE_TEST_SET:
+                print("X_test shape:", X_test.shape)
             print("\n")
         else:
             # Standardized Data
             selector = SelectKBest(mutual_info_classif, k=3000)
             X_train_standardized = selector.fit_transform(X_train_standardized, y_train)
             X_val_standardized = selector.transform(X_val_standardized)
-            X_test_standardized = selector.transform(X_test_standardized)
+            if USE_TEST_SET:
+                X_test_standardized = selector.transform(X_test_standardized)
 
             print("Standardized Data")
             print("X_train_standardized shape:", X_train_standardized.shape)
             print("X_val_standardized shape:", X_val_standardized.shape)
-            print("X_test_standardized shape:", X_test_standardized.shape)
+            if USE_TEST_SET:
+                print("X_test_standardized shape:", X_test_standardized.shape)
             print("\n")
 
 
@@ -129,12 +141,14 @@ if USE_RAW_DATA:
     # Raw data
     X_train, y_train = shuffle(X_train, y_train, random_state=0)
     X_val, y_val = shuffle(X_val, y_val, random_state=0)
-    X_test, y_test = shuffle(X_test, y_test, random_state=0)
+    if USE_TEST_SET:
+        X_test, y_test = shuffle(X_test, y_test, random_state=0)
 else:
     # Standardized data
     X_train, y_train = shuffle(X_train_standardized, y_train, random_state=0)
     X_val, y_val = shuffle(X_val_standardized, y_val, random_state=0)
-    X_test, y_test = shuffle(X_test_standardized, y_test, random_state=0)
+    if USE_TEST_SET:
+        X_test, y_test = shuffle(X_test_standardized, y_test, random_state=0)
 
 
 # Machine Learning Approach
@@ -224,30 +238,33 @@ num_epochs = 1000
 history = clf.fit(X_train, y_train, epochs=num_epochs, batch_size=128, validation_data=(X_val, y_val))
 
 
-# # Plot loss and accuracy
-# epochs = range(num_epochs)
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# # Plot loss
-# plt.plot(epochs, loss, 'b', label='Training loss')
-# plt.plot(epochs, val_loss, 'r', label='Validation loss')
-# plt.title('Loss plot')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.show()
-#
-# # Plot accuracy
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
-# plt.plot(range(num_epochs), acc, 'b', label='Training accuracy')
-# plt.plot(range(num_epochs), val_acc, 'r', label='Validation accuracy')
-# plt.title('Accuracy plot')
-# plt.xlabel('Epochs')
-# plt.ylabel('Accuracy')
-# plt.legend()
-# plt.show()
+try:
+    # Plot loss and accuracy
+    epochs = range(num_epochs)
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    # Plot loss
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Loss plot')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    # Plot accuracy
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    plt.plot(range(num_epochs), acc, 'b', label='Training accuracy')
+    plt.plot(range(num_epochs), val_acc, 'r', label='Validation accuracy')
+    plt.title('Accuracy plot')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+except:
+    pass
 
 
 # Results
